aee64d8ef61728ef8b0c027ecc234ee4ed55f0a5
Abhishek-mac <jkm@JKMs-MacBook-Pro.local>
2022-06-27 22:29:21 +0530

    my first commit

---
+++ src/Row.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import Youtube from "react-youtube";
+import "./Row.css";
+import movieTrailer from "movie-trailer";
+
+const base_Url = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  // Options for react-youtube
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  // A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    // if [], runs once when the row loads, and don't run again
+    async function fetchData() {
+      // console.log('axios =',axios.get(fetchUrl    ));
+      //    console.log('test =',`${axios}${fetchUrl}`)
+      const request = await axios.get(fetchUrl);
+
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {/*  several row_poster */}
+
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_Url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            onClick={() => {
+              handleClick(movie);
+            }}
+            // onClick={handleClick(movie)}
+          />
+        ))}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
